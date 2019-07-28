2685f5b03524e1d29cb155ec165272b35478cd6f
Harsh <harshanurag04@gmail.com>
2019-07-28 00:19:14 -0500

    theme change

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = '/home/harsh/Documents/personal_webpage/pelican-themes/hyde'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
